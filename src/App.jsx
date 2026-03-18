4e82dbd93d24ed59a68b1d5af2af675f2d4aad84
NikitaDev <nikitapaul229@gmail.com>
2026-03-18 16:27:48 -0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Header from "./components/Header";
+function App() {
+  return (
+    <div>
+      <Header />
+      <h1>Daily Task Tracker</h1>
+      <p>Hello LaunchCode 🚀</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+function Header() {
+  return (
+    <nav>
+      <h2>TaskTracker</h2>
+      <div>
+        <a href="#">Home</a>
+        <a href="#">About</a>
+      </div>
+    </nav>
+  );
+}
+
+export default Header;
